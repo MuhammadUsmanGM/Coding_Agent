93e29117af10b50df231c27d68b136b4a3fc35ce
MuhammadUsmanGM <MuhammadUsmanGM@users.noreply.github.com>
2025-11-29 08:14:37 +0000

    Set background image and update page title and favicon

--- Codeius-GUI/vite.config.js
+++ Codeius-GUI/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true, // Automatically open the browser
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8080', // Default Flask backend port
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
