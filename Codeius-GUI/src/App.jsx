b2e7e2b2192dff4a20f29f2f2743a666a191a4a2
MuhammadUsmanGM <MuhammadUsmanGM@users.noreply.github.com>
2025-11-30 07:12:27 +0000

    improved the chat ui

--- Codeius-GUI/src/App.jsx
+++ Codeius-GUI/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import InputField from './components/InputField/InputField'
@@ -15,12 +15,57 @@ function App() {
     }
   ]);
 
+  const messagesEndRef = useRef(null);
+  const chatContainerRef = useRef(null);
+
+  // Scroll to bottom when messages change, but only if user hasn't scrolled up
+  const shouldAutoScroll = useRef(true);
+
+  useEffect(() => {
+    const container = chatContainerRef.current;
+    if (container) {
+      // Check if user is near the bottom (within 100px of the bottom)
+      const isNearBottom = container.scrollHeight - container.clientHeight - container.scrollTop < 100;
+
+      // Update ref to track if we should auto-scroll
+      shouldAutoScroll.current = isNearBottom;
+
+      if (shouldAutoScroll.current) {
+        // Scroll to bottom smoothly
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [messages]);
+
+  // Add scroll listener to detect when user scrolls up
+  useEffect(() => {
+    const container = chatContainerRef.current;
+
+    const handleScroll = () => {
+      if (container) {
+        // Check if user has scrolled up significantly (more than 100px from bottom)
+        const isNearBottom = container.scrollHeight - container.clientHeight - container.scrollTop < 100;
+        shouldAutoScroll.current = isNearBottom;
+      }
+    };
+
+    if (container) {
+      container.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       <Sidebar />
       {/* Chat bubbles appear on the background */}
-      <div className="chat-bubbles-container">
+      <div className="chat-bubbles-container" ref={chatContainerRef}>
         {messages.map((message) => (
           <ChatBubble
             key={message.id}
@@ -29,6 +74,7 @@ function App() {
             timestamp={message.timestamp.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
           />
         ))}
+        <div ref={messagesEndRef} />
       </div>
       {/* The background image remains visible as the background of the App div */}
       <InputField setMessages={setMessages} messages={messages} />
